ee09e4f9671a98818e46b4576f3160a4b037b2ec
Marouane <manaa.marouane@gmail.com>
2023-05-17 20:56:31 +0200

    fix(examples): fix fast api example

--- examples/fast_api_example.py
+++ examples/fast_api_example.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from sinch import ClientAsync
+from sinch import Client
 
 """
 Run with: uvicorn fast_api_example:app --reload
@@ -8,16 +9,16 @@ Run with: uvicorn fast_api_example:app --reload
 app = FastAPI()
 
 sinch_client = ClientAsync(
-    key_id="Spodek",
-    key_secret="wKatowicach"
+    key_id="KEY_ID",
+    key_secret="KEY_SECRET",
+    project_id="PROJECT_ID"
 )
 
 
 @app.get("/available_numbers")
 async def project():
-    numbers = await sinch_client.numbers.list_available_numbers(
+    numbers = await sinch_client.numbers.available.list(
         region_code="US",
         number_type="LOCAL",
-        project_id="e15b2651-daac-4ccb-92e8-e3066d1d033b"
     )
     return {"available_numbers": numbers.available_numbers}
