5b8cc04a1fabe2429ac3fc4363fd022dc8e21123
650elx <bprokop02@gmail.com>
2023-09-22 16:16:47 +0200

    feat(MyPy): annotate Token Manager

--- sinch/core/token_manager.py
+++ sinch/core/token_manager.py
@@ -1,8 +1,12 @@
 from enum import Enum
 from abc import ABC, abstractmethod
+from typing import Optional
+
 from sinch.domains.authentication.models.authentication import OAuthToken
 from sinch.domains.authentication.endpoints.oauth import OAuthEndpoint
 from sinch.core.exceptions import ValidationException
+from sinch.core.clients.sinch_client_base import ClientBase
+from sinch.core.models.http_response import HTTPResponse
 
 
 class TokenState(Enum):
@@ -12,24 +16,24 @@ class TokenState(Enum):
 
 
 class TokenManagerBase(ABC):
-    def __init__(self, sinch):
+    def __init__(self, sinch: ClientBase):
         self.sinch = sinch
-        self.token = None
+        self.token: Optional[OAuthToken] = None
         self.token_state = TokenState.INVALID
 
     @abstractmethod
-    def get_auth_token(self) -> OAuthToken:
+    def get_auth_token(self) -> Optional[OAuthToken]:
         pass
 
     def invalidate_expired_token(self):
         self.token = None
         self.token_state = TokenState.EXPIRED
 
-    def handle_invalid_token(self, http_response):
+    def handle_invalid_token(self, http_response: HTTPResponse):
         if http_response.headers.get("www-authenticate") and "expired" in http_response.headers["www-authenticate"]:
             self.invalidate_expired_token()
 
-    def set_auth_token(self, token) -> None:
+    def set_auth_token(self, token: dict) -> None:
         try:
             self.token = OAuthToken(**token)
             self.token_state = TokenState.VALID
@@ -42,7 +46,7 @@ class TokenManagerBase(ABC):
 
 
 class TokenManager(TokenManagerBase):
-    def get_auth_token(self) -> OAuthToken:
+    def get_auth_token(self) -> Optional[OAuthToken]:
         if self.token:
             return self.token
 
@@ -52,7 +56,7 @@ class TokenManager(TokenManagerBase):
 
 
 class TokenManagerAsync(TokenManagerBase):
-    async def get_auth_token(self) -> OAuthToken:
+    async def get_auth_token(self) -> Optional[OAuthToken]:
         if self.token:
             return self.token
 
