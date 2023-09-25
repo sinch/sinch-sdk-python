49d2696940754f307226c2d3c4414bb2d7ea0cd8
650elx <bprokop02@gmail.com>
2023-09-25 14:28:01 +0200

    feat(MyPy): MyPy redeclaration warning fixed

--- sinch/core/adapters/requests_http_transport.py
+++ sinch/core/adapters/requests_http_transport.py
@@ -12,21 +12,21 @@ class HTTPTransportRequests(HTTPTransport):
 
     def request(self, endpoint: HTTPEndpoint) -> HTTPResponse:
         request_data: HttpRequest = self.prepare_request(endpoint)
-        request_data: HttpRequest = self.authenticate(endpoint, request_data)
+        request_data_with_auth: HttpRequest = self.authenticate(endpoint, request_data)
 
         self.sinch.configuration.logger.debug(
-            f"Sync HTTP {request_data.http_method} call with headers:"
-            f" {request_data.headers} and body: {request_data.request_body} to URL: {request_data.url}"
+            f"Sync HTTP {request_data_with_auth.http_method} call with headers:"
+            f" {request_data_with_auth.headers} and body: {request_data_with_auth.request_body} to URL: {request_data_with_auth.url}"
         )
 
         response = self.session.request(
-            method=request_data.http_method,
-            url=request_data.url,
-            data=request_data.request_body,
-            auth=request_data.auth,
-            headers=request_data.headers,
+            method=request_data_with_auth.http_method,
+            url=request_data_with_auth.url,
+            data=request_data_with_auth.request_body,
+            auth=request_data_with_auth.auth,
+            headers=request_data_with_auth.headers,
             timeout=self.sinch.configuration.connection_timeout,
-            params=request_data.query_params
+            params=request_data_with_auth.query_params
         )
 
         response_body = response.content
@@ -35,7 +35,7 @@ class HTTPTransportRequests(HTTPTransport):
 
         self.sinch.configuration.logger.debug(
             f"Sync HTTP {response.status_code} response with headers: {response.headers}"
-            f"and body: {response_body} from URL: {request_data.url}"
+            f"and body: {response_body} from URL: {request_data_with_auth.url}"
         )
 
         return self.handle_response(
