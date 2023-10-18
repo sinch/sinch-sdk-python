242ed3e1c10d667fcc9a031b895b5de9fc70f7be
650elx <bprokop02@gmail.com>
2023-10-18 16:17:13 +0200

    fix(MyPy): str -> bool

--- sinch/core/exceptions.py
+++ sinch/core/exceptions.py
@@ -1,5 +1,5 @@
 class SinchException(Exception):
-    def __init__(self, message: str, response, is_from_server: str):
+    def __init__(self, message: str, response, is_from_server: bool):
         self.is_from_server = is_from_server
         self.response_status_code = response.status_code if response else None
         self.http_response = response
