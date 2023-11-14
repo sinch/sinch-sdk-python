3855f070d0e7568fecc86673945bd09cf9cecd0c
650elx <bprokop02@gmail.com>
2023-11-14 13:47:24 +0100

    feat(Aiohttp): preserve Client Sessions between the http calls

--- sinch/core/adapters/asyncio_http_adapter.py
+++ sinch/core/adapters/asyncio_http_adapter.py
@@ -4,6 +4,8 @@ from sinch.core.ports.http_transport import AsyncHTTPTransport, HttpRequest
 from sinch.core.endpoint import HTTPEndpoint
 from sinch.core.models.http_response import HTTPResponse
 
+aiohttp_session = aiohttp.ClientSession()
+
 
 class HTTPTransportAioHTTP(AsyncHTTPTransport):
     async def request(self, endpoint: HTTPEndpoint) -> HTTPResponse:
@@ -15,33 +17,32 @@ class HTTPTransportAioHTTP(AsyncHTTPTransport):
             f" {request_data.headers} and body: {request_data.request_body} to URL: {request_data.url}"
         )
 
-        async with aiohttp.ClientSession() as session:
-            async with session.request(
-                method=request_data.http_method,
-                headers=request_data.headers,
-                url=request_data.url,
-                data=request_data.request_body,
-                auth=request_data.auth,
-                params=request_data.query_params,
-                timeout=aiohttp.ClientTimeout(
-                    total=self.sinch.configuration.connection_timeout
-                )
-            ) as response:
+        async with aiohttp_session.request(
+            method=request_data.http_method,
+            headers=request_data.headers,
+            url=request_data.url,
+            data=request_data.request_body,
+            auth=request_data.auth,
+            params=request_data.query_params,
+            timeout=aiohttp.ClientTimeout(
+                total=self.sinch.configuration.connection_timeout
+            )
+        ) as response:
 
-                response_body = await response.read()
-                if response_body:
-                    response_body = json.loads(response_body)
+            response_body = await response.read()
+            if response_body:
+                response_body = json.loads(response_body)
 
-                self.sinch.configuration.logger.debug(
-                    f"Async HTTP {response.status} response with headers: {response.headers}"
-                    f"and body: {response_body} from URL: {request_data.url}"
-                )
+            self.sinch.configuration.logger.debug(
+                f"Async HTTP {response.status} response with headers: {response.headers}"
+                f"and body: {response_body} from URL: {request_data.url}"
+            )
 
-                return await self.handle_response(
-                    endpoint=endpoint,
-                    http_response=HTTPResponse(
-                        status_code=response.status,
-                        body=response_body,
-                        headers=response.headers
-                    )
+            return await self.handle_response(
+                endpoint=endpoint,
+                http_response=HTTPResponse(
+                    status_code=response.status,
+                    body=response_body,
+                    headers=response.headers
                 )
+            )
