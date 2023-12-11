33dde7fea5b5cded8ac151d08b0632c04cb6e795
Bartosz Prokop <bprokop02@gmail.com>
2023-12-11 16:06:30 +0100

    Numbers API: Callback configuration endpoints added.

---
+++ sinch/domains/numbers/endpoints/callbacks/get_configuration.py
@@ -0,0 +1,27 @@
+from sinch.core.models.http_response import HTTPResponse
+from sinch.domains.numbers.endpoints.numbers_endpoint import NumbersEndpoint
+from sinch.core.enums import HTTPAuthentication, HTTPMethods
+from sinch.domains.numbers.models.callbacks.responses import GetNumbersCallbackConfigurationResponse
+
+
+class GetNumbersCallbackConfigurationEndpoint(NumbersEndpoint):
+    ENDPOINT_URL = "{origin}/v1/projects/{project_id}/callbackConfiguration"
+    HTTP_METHOD = HTTPMethods.GET.value
+    HTTP_AUTHENTICATION = HTTPAuthentication.OAUTH.value
+
+    def __init__(self, project_id: str):
+        super().__init__(project_id, None)
+        self.project_id = project_id
+
+    def build_url(self, sinch):
+        return self.ENDPOINT_URL.format(
+            origin=sinch.configuration.numbers_origin,
+            project_id=self.project_id
+        )
+
+    def handle_response(self, response: HTTPResponse) -> GetNumbersCallbackConfigurationResponse:
+        super().handle_response(response)
+        return GetNumbersCallbackConfigurationResponse(
+            project_id=response.body['projectId'],
+            hmac_secret=response.body['hmacSecret']
+        )
