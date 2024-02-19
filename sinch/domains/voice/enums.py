9e057629bc58a1579d191383dd3a1e3ee80745b3
650elx <bprokop02@gmail.com>
2024-02-19 12:31:16 +0100

    feat(Voice): bring back regions enum

--- sinch/domains/voice/enums.py
+++ sinch/domains/voice/enums.py
@@ -5,3 +5,11 @@ class CalloutMethod(Enum):
     TEXT_TO_SPEECH = "ttsCallout"
     CUSTOM = "customCallout"
     CONFERENCE = "conferenceCallout"
+
+
+class VoiceRegion(Enum):
+    EUROPE = "euc1"
+    NORTH_AMERICA = "use1"
+    SOUTH_AMERICA = "sae1"
+    SOUTH_EAST_ASIA_1 = "apse1"
+    SOUTH_EAST_ASIA_2 = "apse2"
