b78427b165c21d11dc2b4bddfaf0b13682c9bda4
650elx <bprokop02@gmail.com>
2024-03-07 13:56:03 +0100

    test(Voice): async tests added to callouts

--- tests/e2e/voice/callouts/test_callout.py
+++ tests/e2e/voice/callouts/test_callout.py
@@ -53,12 +53,45 @@ def test_conference_callout(
     assert isinstance(conference_callout_response, VoiceCalloutResponse)
 
 
+async def test_conference_callout_async(
+    sinch_client_async,
+    phone_number,
+    voice_origin_phone_number,
+    conference_id
+):
+    conference_callout_response = await sinch_client_async.voice.callouts.conference(
+        conference_id=conference_id,
+        destination={
+            "type": "number",
+            "endpoint": phone_number
+        },
+        locale="en-US",
+        cli=voice_origin_phone_number
+    )
+    assert isinstance(conference_callout_response, VoiceCalloutResponse)
+
+
 def test_custom_callout(
     sinch_client_sync,
     phone_number,
     voice_origin_phone_number
 ):
     custom_callout_response = sinch_client_sync.voice.callouts.custom(
+        destination={
+            "type": "number",
+            "endpoint": phone_number
+        },
+        cli=voice_origin_phone_number
+    )
+    assert isinstance(custom_callout_response, VoiceCalloutResponse)
+
+
+async def test_custom_callout_async(
+    sinch_client_async,
+    phone_number,
+    voice_origin_phone_number
+):
+    custom_callout_response = await sinch_client_async.voice.callouts.custom(
         destination={
             "type": "number",
             "endpoint": phone_number
