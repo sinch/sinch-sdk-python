379d4a6d9ada621e5996a8dfcb54ce74ec6f80e7
650elx <bprokop02@gmail.com>
2024-05-20 19:50:35 +0200

    GA: version bump

--- sinch/__init__.py
+++ sinch/__init__.py
@@ -1,7 +1,7 @@
 """ Sinch Python SDK
 To access Sinch resources, use the Sync or Async version of the Sinch Client.
 """
-__version__ = "0.4.0"
+__version__ = "1.0.0"
 
 from sinch.core.clients.sinch_client_sync import SinchClient
 from sinch.core.clients.sinch_client_async import SinchClientAsync
