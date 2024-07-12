35bbc8d5da90a29cc0f1fec48de0180376b53e27
650elx <bprokop02@gmail.com>
2024-07-12 16:00:12 +0200

    feat(SMS): more e2e tests

--- tests/e2e/sms/groups/test_get_phone_numbers_for_group.py
+++ tests/e2e/sms/groups/test_get_phone_numbers_for_group.py
@@ -1,6 +1,18 @@
 from sinch.domains.sms.models.groups.responses import SinchGetSMSGroupPhoneNumbersResponse
 from sinch.core.enums import HTTPAuthentication
 
+
+def test_get_group_phone_numbers_sms_with_service_plan_id(sinch_client_sync):
+    sinch_client_sync.configuration.sms_authentication_method = HTTPAuthentication.SMS_TOKEN.value
+    list_group_response = sinch_client_sync.sms.groups.list()
+
+    get_group_response = sinch_client_sync.sms.groups.get_group_phone_numbers(
+        group_id=list_group_response.result.groups[0].id
+    )
+
+    assert isinstance(get_group_response, SinchGetSMSGroupPhoneNumbersResponse)
+    assert get_group_response.phone_numbers
+
 def test_get_group_phone_numbers_sms(sinch_client_sync):
     list_group_response = sinch_client_sync.sms.groups.list()
 
--- tests/e2e/sms/groups/test_replace_group.py
+++ tests/e2e/sms/groups/test_replace_group.py
@@ -2,6 +2,17 @@ from sinch.domains.sms.models.groups.responses import ReplaceSMSGroupResponse
 from sinch.core.enums import HTTPAuthentication
 
 
+def test_replace_sms_group_with_service_plan_id(sinch_client_sync):
+    sinch_client_sync.configuration.sms_authentication_method = HTTPAuthentication.SMS_TOKEN.value
+    list_group_response = sinch_client_sync.sms.groups.list()
+
+    replace_group_response = sinch_client_sync.sms.groups.replace(
+        group_id=list_group_response.result.groups[0].id,
+        members=["48111111111"]
+    )
+    assert isinstance(replace_group_response, ReplaceSMSGroupResponse)
+
+
 def test_replace_sms_group(sinch_client_sync):
     list_group_response = sinch_client_sync.sms.groups.list()
 
