a4e985172779977673b0a67bafcabedf320226e1
650elx <bprokop02@gmail.com>
2024-08-14 12:24:21 +0200

    feat(Verification): more e2e tests

--- tests/e2e/verification/test_report_verification_using_id.py
+++ tests/e2e/verification/test_report_verification_using_id.py
@@ -3,7 +3,6 @@ from sinch.domains.verification.models.responses import ReportVerificationByIdRe
 
 def test_report_verification_using_id_and_sms(
     sinch_client_sync,
-    phone_number,
     verification_id
 ):
     verification_response = sinch_client_sync.verification.verifications.report_sms_by_id(
@@ -15,7 +14,6 @@ def test_report_verification_using_id_and_sms(
 
 def test_report_verification_using_id_and_flash_call(
     sinch_client_sync,
-    phone_number,
     verification_id
 ):
     verification_response = sinch_client_sync.verification.verifications.report_flash_call_by_id(
@@ -27,7 +25,6 @@ def test_report_verification_using_id_and_flash_call(
 
 def test_report_verification_using_id_and_and_phone_call(
     sinch_client_sync,
-    phone_number,
     verification_id
 ):
     verification_response = sinch_client_sync.verification.verifications.report_phone_call_by_id(
@@ -39,50 +36,32 @@ def test_report_verification_using_id_and_and_phone_call(
 
 async def test_report_verification_using_id_and_sms_async(
     sinch_client_async,
-    phone_number,
     verification_id
 ):
     verification_response = await sinch_client_async.verification.verifications.report_sms_by_id(
         id=verification_id,
-        verification_report_request={
-            "method": "sms",
-            "sms": {
-                "code": "2302"
-            }
-        }
+        code="2302"
     )
     assert isinstance(verification_response, ReportVerificationByIdResponse)
 
 
 async def test_report_verification_using_id_and_flash_call_async(
     sinch_client_async,
-    phone_number,
     verification_id
 ):
     verification_response = await sinch_client_async.verification.verifications.report_flash_call_by_id(
         id=verification_id,
-        verification_report_request={
-            "method": "sms",
-            "sms": {
-                "code": "2302"
-            }
-        }
+        cli="2332"
     )
     assert isinstance(verification_response, ReportVerificationByIdResponse)
 
 
 async def test_report_verification_using_id_and_and_phone_call_async(
     sinch_client_async,
-    phone_number,
     verification_id
 ):
     verification_response = await sinch_client_async.verification.verifications.report_phone_call_by_id(
         id=verification_id,
-        verification_report_request={
-            "method": "sms",
-            "sms": {
-                "code": "2302"
-            }
-        }
+        code="2302"
     )
     assert isinstance(verification_response, ReportVerificationByIdResponse)
--- tests/e2e/verification/test_report_verification_using_identity.py
+++ tests/e2e/verification/test_report_verification_using_identity.py
@@ -1,33 +1,72 @@
 from sinch.domains.verification.models.responses import ReportVerificationByIdentityResponse
 
-
 def test_report_verification_using_identity_and_sms(
     sinch_client_sync,
+    verification_id,
     phone_number
 ):
-    verification_response = sinch_client_sync.verification.verifications.report_by_identity(
+    verification_response = sinch_client_sync.verification.verifications.report_sms_by_identity(
         endpoint=phone_number,
-        verification_report_request={
-              "method": "sms",
-              "sms": {
-                "code": "2302"
-              }
-        }
+        code="2302"
+    )
+    assert isinstance(verification_response, ReportVerificationByIdentityResponse)
+
+
+def test_report_verification_using_identity_and_flash_call(
+    sinch_client_sync,
+    verification_id,
+    phone_number
+):
+    verification_response = sinch_client_sync.verification.verifications.report_flash_call_by_identity(
+        endpoint=phone_number,
+        code="2302"
+    )
+    assert isinstance(verification_response, ReportVerificationByIdentityResponse)
+
+
+def test_report_verification_using_identity_and_phone_call(
+    sinch_client_sync,
+    verification_id,
+    phone_number
+):
+    verification_response = sinch_client_sync.verification.verifications.report_phone_call_by_identity(
+        endpoint=phone_number,
+        code="2302"
     )
     assert isinstance(verification_response, ReportVerificationByIdentityResponse)
 
 
 async def test_report_verification_using_identity_and_sms_async(
     sinch_client_async,
+    verification_id,
     phone_number
 ):
-    verification_response = await sinch_client_async.verification.verifications.report_by_identity(
+    verification_response = await sinch_client_async.verification.verifications.report_sms_by_identity(
         endpoint=phone_number,
-        verification_report_request={
-              "method": "sms",
-              "sms": {
-                "code": "2302"
-              }
-        }
+        code="2302"
+    )
+    assert isinstance(verification_response, ReportVerificationByIdentityResponse)
+
+
+async def test_report_verification_using_identity_and_flash_call_async(
+    sinch_client_async,
+    verification_id,
+    phone_number
+):
+    verification_response = await sinch_client_async.verification.verifications.report_flash_call_by_identity(
+        endpoint=phone_number,
+        code="2302"
+    )
+    assert isinstance(verification_response, ReportVerificationByIdentityResponse)
+
+
+async def test_report_verification_using_identity_and_phone_call_async(
+    sinch_client_async,
+    verification_id,
+    phone_number
+):
+    verification_response = await sinch_client_async.verification.verifications.report_phone_call_by_identity(
+        endpoint=phone_number,
+        code="2302"
     )
     assert isinstance(verification_response, ReportVerificationByIdentityResponse)
