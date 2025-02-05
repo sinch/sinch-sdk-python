4b3ed1fcbc707d73ea1169ec0c5b36b38897c4e7
Antoine SEIN <142824551+asein-sinch@users.noreply.github.com>
2025-02-05 15:06:45 +0100

    Refactor abstract endpoint classes

--- sinch/domains/numbers/endpoints/numbers_endpoint.py
+++ sinch/domains/numbers/endpoints/numbers_endpoint.py
@@ -1,33 +1,19 @@
 import json
+from abc import ABC
 from pydantic import BaseModel
+from typing import TypeVar, Type
 from sinch.core.models.http_response import HTTPResponse
 from sinch.core.endpoint import HTTPEndpoint
 from sinch.domains.numbers.exceptions import NumbersException
+from sinch.domains.numbers.models.numbers import NotFoundError
 
 
-class NumbersEndpoint(HTTPEndpoint):
-    """
-        A base class for all endpoints, providing reusable logic for URL building
-        and response parsing.
-        """
-    ENDPOINT_URL: str = ""
-    HTTP_METHOD: str = ""
-    HTTP_AUTHENTICATION: str = ""
+class NumbersEndpoint(HTTPEndpoint, ABC):
 
     def __init__(self, project_id: str, request_data: object):
-        self.project_id = project_id
-        self.request_data = request_data
+        super().__init__(project_id, request_data)
 
     def build_url(self, sinch) -> str:
-        """
-        Constructs the URL for the endpoint.
-
-        Args:
-            sinch: The Sinch client instance.
-
-        Returns:
-            str: Fully constructed endpoint URL.
-        """
         if not self.ENDPOINT_URL:
             raise NotImplementedError("ENDPOINT_URL must be defined in the subclass.")
 
@@ -48,7 +34,9 @@ class NumbersEndpoint(HTTPEndpoint):
         request_data = self.request_data.model_dump(by_alias=True, exclude_none=True)
         return json.dumps(request_data)
 
-    def process_response_model(self, response_body: dict, response_model: type[BaseModel]) -> BaseModel:
+    BM = TypeVar("BM", bound=BaseModel)
+
+    def process_response_model(self, response_body: dict, response_model: Type[BM]) -> BM:
         """
         Processes the response body and maps it to a response model.
 
@@ -65,6 +53,9 @@ class NumbersEndpoint(HTTPEndpoint):
             raise ValueError(f"Invalid response structure: {e}") from e
 
     def handle_response(self, response: HTTPResponse):
+        if response.status_code == 404:
+            return NotFoundError(**response.body['error'])
+
         if response.status_code >= 400:
             raise NumbersException(
                 message=f"{response.body['error'].get('message')}  {response.body['error'].get('status')}",
