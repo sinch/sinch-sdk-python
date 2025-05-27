abb3aaaf2656800e32da7b4fda3682811509a3d6
Jessica Matsuoka <jessica.akemi.matsuoka@sinch.com>
2025-05-27 14:05:18 +0200

    chore: rename error model

--- sinch/domains/numbers/models/v1/errors/__init__.py
+++ sinch/domains/numbers/models/v1/errors/__init__.py
@@ -1,4 +1,4 @@
-from sinch.domains.numbers.models.v1.errors.not_found import NotFoundError
+from sinch.domains.numbers.models.v1.errors.not_found_error import NotFoundError
 from sinch.domains.numbers.models.v1.errors.error_details import ErrorDetails
 
 __all__ = [
