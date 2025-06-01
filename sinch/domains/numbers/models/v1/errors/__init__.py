510040ce6799eac574a928a7ad8938fa34501752
Jessica Matsuoka <jessica.akemi.matsuoka@sinch.com>
2025-06-01 15:57:57 +0200

    chore: rename error details

--- sinch/domains/numbers/models/v1/errors/__init__.py
+++ sinch/domains/numbers/models/v1/errors/__init__.py
@@ -1,7 +1,7 @@
 from sinch.domains.numbers.models.v1.errors.not_found_error import NotFoundError
-from sinch.domains.numbers.models.v1.errors.error_details import ErrorDetails
+from sinch.domains.numbers.models.v1.errors.not_found_error_details import NotFoundErrorDetails
 
 __all__ = [
     "NotFoundError",
-    "ErrorDetails",
+    "NotFoundErrorDetails",
 ]
