d7a3b17483b14215150b768830f9dee4ca9b41ee
Jessica Matsuoka <jessica.akemi.matsuoka@sinch.com>
2025-06-02 14:27:35 +0200

    chore: rename model filename

--- sinch/domains/numbers/models/v1/response/__init__.py
+++ sinch/domains/numbers/models/v1/response/__init__.py
@@ -1,7 +1,7 @@
 from sinch.domains.numbers.models.v1.response.active_number import ActiveNumber
 from sinch.domains.numbers.models.v1.response.available_number import AvailableNumber
 from sinch.domains.numbers.models.v1.response.available_region import AvailableRegion
-from sinch.domains.numbers.models.v1.response.numbers_callback import CallbackConfigurationResponse
+from sinch.domains.numbers.models.v1.response.callback_configuration_response import CallbackConfigurationResponse
 
 __all__ = [
     "ActiveNumber",
