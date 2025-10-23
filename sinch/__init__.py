e16baa4894ed3520eb5fc23593d57390c7738257
Jessica Matsuoka <jessica.akemi.matsuoka@sinch.com>
2025-10-23 15:19:13 +0200

    relese(ci): Add Python 3.13 and 3.14 support (#89)
    Co-authored by: @vincentdavis

--- sinch/__init__.py
+++ sinch/__init__.py
@@ -1,7 +1,7 @@
 """ Sinch Python SDK
 To access Sinch resources, use the Sync or Async version of the Sinch Client.
 """
-__version__ = "1.1.3"
+__version__ = "1.1.4"
 
 from sinch.core.clients.sinch_client_sync import SinchClient
 from sinch.core.clients.sinch_client_async import SinchClientAsync
