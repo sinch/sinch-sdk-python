45e0f84bc4fd7a1c1e6fdf424c1a6f70a3e6e931
Jessica Matsuoka <jessica.akemi.matsuoka@sinch.com>
2026-01-08 19:38:39 +0100

    DEVEXP-794: Add Conversation Region

--- tests/unit/test_user_agent_header.py
+++ tests/unit/test_user_agent_header.py
@@ -1,10 +1,18 @@
-#from sinch.domains.conversation.endpoints.app.delete_app import DeleteConversationAppEndpoint
-#from sinch.domains.conversation.models.app.requests import DeleteConversationAppRequest
+from sinch.domains.conversation.api.v1.internal.messages_endpoints import GetMessageEndpoint
+from sinch.domains.conversation.models.v1.messages.internal.request import MessageIdRequest
 
-# TODO: Reimplement test when DeleteConversationAppEndpoint is functional
-def test_user_agent_header_creation(sinch_client_sync):
-    pass
-    # endpoint = DeleteConversationAppRequest(app_id="42")
-    # http_endpoint = DeleteConversationAppEndpoint(sinch_client_sync, endpoint)
-    # http_request = sinch_client_sync.configuration.transport.prepare_request(http_endpoint)
-    # assert "User-Agent" in http_request.headers
+
+def test_user_agent_header_creation_expects_to_be_included(sinch_client_sync):
+    """Test that User-Agent header is created when preparing a conversation endpoint request"""
+    sinch_client_sync.configuration.conversation_region = "eu"
+    
+    request_data = MessageIdRequest(message_id="test_message_id")
+    http_endpoint = GetMessageEndpoint(
+        project_id=sinch_client_sync.configuration.project_id,
+        request_data=request_data
+    )
+    
+    http_request = sinch_client_sync.configuration.transport.prepare_request(http_endpoint)
+    
+    assert "User-Agent" in http_request.headers
+    assert "sinch-sdk" in http_request.headers["User-Agent"]
