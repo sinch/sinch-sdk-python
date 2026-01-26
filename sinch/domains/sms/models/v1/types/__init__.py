f910f5b0269a8ac127abd2b9b9e70da777274928
Jessica Matsuoka <jessica.akemi.matsuoka@sinch.com>
2026-01-26 11:08:30 +0100

    add sms typeddict

--- sinch/domains/sms/models/v1/types/__init__.py
+++ sinch/domains/sms/models/v1/types/__init__.py
@@ -8,6 +8,7 @@ from sinch.domains.sms.models.v1.types.delivery_status_type import (
     DeliveryStatusType,
 )
 from sinch.domains.sms.models.v1.types.encoding_type import EncodingType
+from sinch.domains.sms.models.v1.types.media_body_dict import MediaBodyDict
 from sinch.domains.sms.models.v1.types.recipient_delivery_report_type import (
     RecipientDeliveryReportType,
 )
@@ -18,6 +19,7 @@ __all__ = [
     "DeliveryReportType",
     "DeliveryStatusType",
     "EncodingType",
+    "MediaBodyDict",
     "RecipientDeliveryReportType",
 ]
 
