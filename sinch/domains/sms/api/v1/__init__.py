db0a8ff7252b1149122528ee0f223a3f0cf44745
Marcos Lozano Romero <marcos.lozano01.sinch@gmail.com>
2026-06-05 08:27:57 +0200

    feat(sms): missing Groups in apis __init__

--- sinch/domains/sms/api/v1/__init__.py
+++ sinch/domains/sms/api/v1/__init__.py
@@ -1,7 +1,9 @@
 from sinch.domains.sms.api.v1.batches_apis import Batches
 from sinch.domains.sms.api.v1.delivery_reports_apis import DeliveryReports
+from sinch.domains.sms.api.v1.groups_apis import Groups
 
 __all__ = [
     "Batches",
     "DeliveryReports",
+    "Groups",
 ]
