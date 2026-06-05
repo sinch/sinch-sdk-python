e0bd2bf33b7121e98eb2ee06480792660d3f0c52
Marcos Lozano Romero <marcos.lozano01.sinch@gmail.com>
2026-06-05 08:41:54 +0200

    feat(sms) add inbounds in __init__

--- sinch/domains/sms/api/v1/__init__.py
+++ sinch/domains/sms/api/v1/__init__.py
@@ -1,7 +1,9 @@
 from sinch.domains.sms.api.v1.batches_apis import Batches
 from sinch.domains.sms.api.v1.delivery_reports_apis import DeliveryReports
+from sinch.domains.sms.api.v1.inbounds_apis import Inbounds
 
 __all__ = [
     "Batches",
     "DeliveryReports",
+    "Inbounds",
 ]
